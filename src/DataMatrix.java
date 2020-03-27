55a3d06f0047b3ef86587408965dca8f6c31eecc
apainintheneck <apainintheneck@gmail.com>
2020-03-27 14:38:21 -0700

    Added stubs for DataMatrix.java class.
    These are the basic stubs. I might have missed one or two though. I
    labeled each method or group of methods with each persons name.

---
+++ src/DataMatrix.java
@@ -0,0 +1,77 @@
+//Stubs for the DataMatrix Class
+//Labeled who is doing which sections.
+public class DataMatrix implements BarcodeIO
+{
+   //DATA - Bryce
+   
+   //Constructors - Bryce
+   
+   //Accessors - Bryce
+   
+   @Override
+   //Jeannie
+   public boolean scan(BarcodeImage bc)
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   //Harsandeep
+   public boolean readText(String text)
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   //Jeannie
+   public boolean generateImageFromText()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   //Harsandeep
+   public boolean translateImageToText()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   //Jeannie
+   public void displayTextToConsole()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   //Bryce
+   public void displayImageToConsole()
+   {
+      // TODO Auto-generated method stub
+
+   }
+   
+   //Harsandeep
+   private void cleanImage()
+   {
+      
+   }
+   
+   //Bryce
+   private int computeSignalWidth()
+   {
+      
+   }
+   
+   //Bryce
+   private int computeSignlHeight()
+   {
+      
+   }
+
+}
