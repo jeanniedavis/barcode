230e2b57c3a3cc82a74b2aca426be8272679c839
apainintheneck <apainintheneck@gmail.com>
2020-03-27 12:02:46 -0700

    Added more methods to BarcodeImage class.
    Added:
    BarcodeImage() -Default constructor
    BarcodeImage(String[] strData) -Another Constructor
    CheckSize(String[] data) -private method to check for valid strData.

--- src/BarcodeImage.java
+++ src/BarcodeImage.java
@@ -9,14 +9,64 @@ public class BarcodeImage implements Cloneable
    {
       imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
       //Set all values in array to false
+      for(int row = 0; row < MAX_HEIGHT; row++)
+      {
+         for(int col = 0; col < MAX_WIDTH; col++)
+         {
+            imageData[row][col] = false;
+         }
+      }
    }
    
    public BarcodeImage(String[] strData)
    {
-      //Split string using delimiter
-      //Find out dimensions of array
+      imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
       int height;
-      int width;
+      int width = 0;
+      
+      //Check to see if array strData has valid dimensions.
+      if(checkSize(strData) == true)
+      {
+         height = strData.length;
+      }
+      else
+      {
+         height = 0;
+      }
+      
+      int col;
+      int arrayIndex = 0; //Index for strData array
+      for(int row = MAX_HEIGHT; row >= 0; row--)
+      {
+         col = 0;
+         
+         //Set columns according to strData input
+         if(row < height) 
+         {
+            width = strData[arrayIndex].length();
+            
+            for(; col < width; col++)
+            {
+               if(strData[arrayIndex].charAt(col) == ' ')
+               {
+                  imageData[row][col] = false;
+               }
+               else
+               {
+                  imageData[row][col] = true;
+               }
+            }
+            
+            arrayIndex++;
+         }
+         
+         //Set remaining columns to false
+         while(col < MAX_WIDTH)
+         {
+            imageData[row][col] = false;
+            col++;
+         }
+      }
    }
    
    boolean getPixel(int row, int col)
@@ -44,17 +94,38 @@ public class BarcodeImage implements Cloneable
       }
    }
    
-   private checkSize(String[] data)
+   private boolean checkSize(String[] data)
+   {
+      //First check is array is null
+      if(data == null)
+      {
+         return false;
+      }
+      
+      //Second check height
+      if(data.length > MAX_HEIGHT || data.length <= 0)
+      {
+         return false;
+      }
+      
+      //Third check width
+      for(String str : data)
+      {
+         if(str == null || str.length() <= 0 || str.length() > MAX_WIDTH)
+         {
+            return false;
+         }
+      }
+      
+      return true;  
+   }
+   
+   public void displayToConsole()
    {
       
    }
    
-   public displayToConsole()
-   {
-      
-   }
-   
-   public clone()
+   public BarcodeImage clone()
    {
       
    }
