12004f9acd5caad8cb98998705ce314ed341c619
apainintheneck <42982186+apainintheneck@users.noreply.github.com>
2020-03-28 00:52:44 -0700

    Phases 1&2 (#1)
    * Added the main file Assig4.java with the sample main() from the spec.
    
    * Added BarcodeIO.java.
    
    Contains BarcadIO interface.
    
    * Working on BarcodeImage class.
    
    Finished Methods:
    -getPixel
    -setPixel
    
    * Added more methods to BarcodeImage class.
    
    Added:
    BarcodeImage() -Default constructor
    BarcodeImage(String[] strData) -Another Constructor
    CheckSize(String[] data) -private method to check for valid strData.
    
    * Added displayToConsole() for testing to BardcodeImage class.
    
    * Added testing for BarcodeImage class.
    
    Testing is in the file testBarcodeImage.java.
    
    * Implemented cloning in BarcodeImage class.
    
    Finished the BarcodeImage.clone() method and added tests in the
    testBarcodeImage.java file to show that it is working.
    
    * Added stubs for DataMatrix.java class.
    
    These are the basic stubs. I might have missed one or two though. I
    labeled each method or group of methods with each persons name.
    
    * Added additional tests to testBarcodeImage.java.
    
    Added tests:
    -default constructor BarcodeImage()
    -BarcodeImage.clone()

---
+++ src/BarcodeImage.java
@@ -0,0 +1,178 @@
+//Holds a barcode image size 30x65 as a 2d array of booleans.
+//If image is invalid, creates blank image.
+public class BarcodeImage implements Cloneable
+{
+   public static final int MAX_HEIGHT = 30;
+   public static final int MAX_WIDTH = 65;
+   private boolean[][] imageData; //Stores pixels of barcode
+
+   //Default Constructor
+   public BarcodeImage()
+   {
+      imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
+      //Set all values in array to false
+      for(int row = 0; row < MAX_HEIGHT; row++)
+      {
+         for(int col = 0; col < MAX_WIDTH; col++)
+         {
+            imageData[row][col] = false;
+         }
+      }
+   }
+   
+   //Constructor
+   public BarcodeImage(String[] strData)
+   {
+      imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
+      int height;
+      int width = 0;
+      
+      //Check to see if array strData has valid dimensions.
+      if(checkSize(strData) == true)
+      {
+         height = strData.length;
+      }
+      else
+      {
+         height = 0;
+      }
+      
+      int col;
+      int arrayIndex = 0; //Index for strData array
+      for(int row = MAX_HEIGHT - 1; row >= 0; row--)
+      {
+         col = 0;
+         
+         //Set columns according to strData input
+         if(row < height) 
+         {
+            width = strData[arrayIndex].length();
+            
+            for(; col < width; col++)
+            {
+               if(strData[arrayIndex].charAt(col) == ' ')
+               {
+                  imageData[row][col] = false;
+               }
+               else
+               {
+                  imageData[row][col] = true;
+               }
+            }
+            
+            arrayIndex++;
+         }
+         
+         //Set remaining columns to false
+         while(col < MAX_WIDTH)
+         {
+            imageData[row][col] = false;
+            col++;
+         }
+      }
+   }
+   
+   boolean getPixel(int row, int col)
+   {
+      if(row < 0 || col < 0 || row >= imageData.length || col >= imageData[0].length)
+      {
+         return false;
+      }
+      else
+      {
+         return imageData[row][col];
+      }
+   }
+   
+   boolean setPixel(int row, int col, boolean value)
+   {
+      if(row < 0 || col < 0 || row >= imageData.length || col >= imageData[0].length)
+      {
+         return false;
+      }
+      else
+      {
+         imageData[row][col] = value;
+         return true;
+      }
+   }
+   
+   //Validates size of incoming String array.
+   private boolean checkSize(String[] data)
+   {
+      //First check is array is null
+      if(data == null)
+      {
+         return false;
+      }
+      
+      //Second check height
+      if(data.length > MAX_HEIGHT || data.length <= 0)
+      {
+         return false;
+      }
+      
+      //Third check width
+      for(String str : data)
+      {
+         if(str == null || str.length() <= 0 || str.length() > MAX_WIDTH)
+         {
+            return false;
+         }
+      }
+      
+      return true;  
+   }
+   
+   //Displays the current barcode image to console.
+   //Used for testing only.
+   public void displayToConsole()
+   {
+      String line;
+      for(int row = MAX_HEIGHT - 1; row >= 0; row--)
+      {
+         line = "";
+         
+         for(int col = 0; col < MAX_WIDTH; col++)
+         {
+            if(imageData[row][col] == true)
+            {
+               line = line + '*';
+            }
+            else
+            {
+               line = line + ' ';
+            }
+         }
+         
+         System.out.println(line);
+      }
+   }
+   
+   //Implements the clonable interface
+   public BarcodeImage clone()
+   {
+      try
+      {
+         //Clone BarcodeImage object
+         BarcodeImage copy = (BarcodeImage)super.clone();
+         
+         //Clone each row of imageData 2d boolean array to copy
+         copy.imageData = new boolean[MAX_HEIGHT][];
+         for(int row = 0; row < MAX_HEIGHT; row++)
+         {
+            copy.imageData[row] = imageData[row].clone();
+         }
+         
+         return copy;
+      } 
+      //If cloning is not supported, return null BarcodeImage object.
+      catch (CloneNotSupportedException e)
+      {
+         BarcodeImage failedCopy = null;
+         return failedCopy;
+      }
+      
+   }
+   
+}
---
+++ src/testBarcodeImage.java
@@ -0,0 +1,99 @@
+//Test main for BarcodeImage.java
+public class testBarcodeImage
+{
+   public static void main(String[] args)
+   {
+      System.out.println("---Test Default Constructor Barcode()---");
+      BarcodeImage defaultImage = new BarcodeImage();
+      System.out.println("Test One:");
+      //Should print out blanks
+      defaultImage.displayToConsole();
+      
+      System.out.println("\n\n---Test Constructor Barcode(String[] strData)---");
+      String[] oneStrArray = 
+         {
+               "        ",
+               "    **  ",
+               "   ***  ",
+               "    **  ",
+               "    **  ",
+               "  ******",
+               "  ******",
+               "        "
+         };
+      
+      BarcodeImage testOne = new BarcodeImage(oneStrArray);
+      System.out.println("Test One:");
+      //Should print out the number one in lower left corner.
+      testOne.displayToConsole();
+      
+      String[] testStrArray = new String[15];
+      for(int row = 0; row < testStrArray.length; row++)
+      {
+         if(row % 2 == 0)
+         {
+            testStrArray[row] = "* * * * * * * * * * * *";
+         }
+         else
+         {
+            testStrArray[row] = " * * * * * * * * * * * ";
+         }
+      }
+      
+      BarcodeImage testTwo = new BarcodeImage(testStrArray);
+      System.out.println("\nTest Two:");
+      //Should print out pattern in lower left corner.
+      testTwo.displayToConsole();
+      
+      String[] testArrayTwo = new String[35];
+      for(int row = 0; row < testArrayTwo.length; row++)
+      {
+         if(row % 2 == 0)
+         {
+            testArrayTwo[row] = "* * * * * * * * * * * *";
+         }
+         else
+         {
+            testArrayTwo[row] = " * * * * * * * * * * * ";
+         }
+      }
+      
+      BarcodeImage testThree = new BarcodeImage(testArrayTwo);
+      System.out.println("\nTest Three:");
+      //Should output blanks because input String array was too large for BarcodeImage class.
+      testThree.displayToConsole();
+      
+      String[] testArrayThree = new String[30];
+      String line = "";
+      for(int col = 0; col < 65; col++)
+      {
+         line = line + '*';
+      }
+      
+      for(int row = 0; row < 30; row++)
+      {
+         testArrayThree[row] = line;
+      }
+      
+      BarcodeImage testFour = new BarcodeImage(testArrayThree);
+      System.out.println("\nTest Four:");
+      //Should output block of stars.
+      testFour.displayToConsole();
+      
+      //Test Clonable
+      BarcodeImage cloneImage = testOne.clone();
+      //Change clone by adding extra line at the bottom of the one.
+      //This shows that it actually made a deep copy.
+      for(int i = 0; i < 10; i++)
+      {
+         cloneImage.setPixel(0, i, true);
+      }
+      System.out.println("\n\n---Testing Clone()---");
+      System.out.println("\nOriginal Hashcode: " + testOne.hashCode());
+      testOne.displayToConsole();
+      System.out.println("\nClone Hashcode: " + cloneImage.hashCode());
+      cloneImage.displayToConsole();
+   }
+   
+   
+}
