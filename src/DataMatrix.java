fdc16aca1411e4b8546123e90ea3f2beca9bfc0a
jeanniedavis <51473352+jeanniedavis@users.noreply.github.com>
2020-03-30 19:03:02 -0700

    geerateImageFromText,scan, and displayTestToConsole

--- src/DataMatrix.java
+++ src/DataMatrix.java
@@ -1,3 +1,4 @@
+import java.lang.Math; 
 //Stubs for the DataMatrix Class
 //Labeled who is doing which sections.
 
@@ -25,7 +26,7 @@ public class DataMatrix implements BarcodeIO
    public DataMatrix(BarcodeImage image)//image only
    {
       text = "";
-      
+
       if(scan(image) == false)
       {
          actualWidth = 0;
@@ -56,10 +57,35 @@ public class DataMatrix implements BarcodeIO
       return actualHeight;
    }
    
-   //Jeannie
+   /**
+    * @author Jeannie
+    @param Barcode - accepts a Barcode Image to create a cloned image to store 
+    * calls cleanImage in order to lower-left justify the Barcode Image 
+    * Also sets the actual width and height 
+    */
    public boolean scan(BarcodeImage bc)
    {
       // TODO Auto-generated method stub
+      try {
+         image = bc.clone();
+         cleanImage();
+         // find actualWidth
+         for (int i = 0; i<bc.MAX_WIDTH; i++){
+            if (!bc.getPixel(bc.MAX_HEIGHT-1, i)){
+               break;
+            }
+            actualWidth++;
+         }
+         // find actualHeight
+         for (int k = bc.MAX_HEIGHT-1; k>=0; k--){
+            if (!bc.getPixel(i, 0)){
+               break;
+            }
+            actualHeight++;
+         }
+      } catch (Exception CloneNotSupportedException) {
+         //TODO: handle exception
+      }
       return false;
    }
 
@@ -71,11 +97,48 @@ public class DataMatrix implements BarcodeIO
       return false;
    }
 
-   //Jeannie
+   /**
+    * @author Jeannie
+    * This method will generate an image from the internal text
+    * This method calls WriteCharToCol which will generate the column for the image
+    */
    public boolean generateImageFromText()
    {
       // TODO Auto-generated method stub
+      //generate column
+      int x;
+      char [] chars = text.toCharArray();
+      for (int k =0; k<chars.length; k++){
+         x = chars[k];
+         WriteCharToCol(k, x);
+      }
       return false;
+   }
+   /**
+    * @author Jeannie
+    * @param col - This is the column to write the character
+    * @param code - This is the integer representation of the character
+    * @return - returns true
+    */
+   private boolean WriteCharToCol(int col, int code){
+      String b = Integer.toBinaryString(code);
+      int counter = 1;
+      for (int i = computeSignalHeight()-1; i >= 0; i--){
+         if (b.length() - counter < 0) {
+            image.setPixel(col, i, false);
+         }
+         else {
+            char var = b.charAt(b.length() -1);
+            if (var == '1'){
+               image.setPixel(col, i, true);
+            }
+            else {
+               image.setPixel(col, i, false);
+            }
+         }
+         counter++;
+      }
+      return true;
    }
 
    //Harsandeep
@@ -85,11 +148,15 @@ public class DataMatrix implements BarcodeIO
       return false;
    }
 
-   //Jeannie
+   //
+   /**
+    * @author Jeannie
+    * This method simply prints out the String text 
+    */
    public void displayTextToConsole()
    {
       // TODO Auto-generated method stub
-
+      System.out.println(text);
    }
 
    //Bryce
@@ -108,13 +175,14 @@ public class DataMatrix implements BarcodeIO
    //Bryce
    private int computeSignalWidth()
    {
+      return 0;
       
    }
    
    //Bryce
    private int computeSignalHeight()
    {
-      
+      return 0;
    }
 
 }
